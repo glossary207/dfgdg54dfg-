41cb53a7769d77102ff86d6ed369037fd3881a3f
glossary207 <36097965+glossary207@users.noreply.github.com>
2023-02-26 11:51:06 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+import openai
+import os
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+# กำหนด API key ของ OpenAI
+openai.api_key = "YOUR_API_KEY"
+
+# กำหนด Channel access token และ Channel secret ของ Linebot
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+
+# กำหนดฟังก์ชันสำหรับการตอบแชท
+def generate_response(input_text):
+    # เรียกใช้ API ของ OpenAI เพื่อสร้างคำตอบแชท
+    prompt = "The following is a conversation with a chatbot. The bot is helpful, creative, clever, and very friendly. " + input_text + "\nBot:"
+    response = openai.Completion.create(
+        engine="davinci", prompt=prompt, max_tokens=1024, n=1, stop=None, temperature=0.5,
+    )
+    return response.choices[0].text.strip()
+
+# กำหนดฟังก์ชันสำหรับการรับข้อความและตอบกลับ
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    input_text = event.message.text
+    output_text = generate_response(input_text)
+    line_bot_api.reply_message(
+        event.reply_token, TextSendMessage(text=output_text)
+    )
+
+# รันโค้ดบน Heroku server
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
